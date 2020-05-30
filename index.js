30365cf14cda9048a5b9baf04ce0cc689b063c0a
dipeshb1 <dipeshb164@gmail.com>
2020-05-30 23:02:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+//import firebase from "firebase"
+//import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import  Firebase from './firebase'
+
+export default function App() {
+  
+  return(   
+        <Firebase/>
+  );
+}
